441a000c8db64a48a26f702f47b13d5e4ae7fc62
Deok Gun Park <intuinno@gmail.com>
2023-03-12 20:22:02 -0500

    Fix run_script

--- hw2/cs285/scripts/run_exp5.sh
+++ hw2/cs285/scripts/run_exp5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gae_lambdas = '0 0.95 0.98 0.99 1'
+gae_lambdas='0 0.95 0.98 0.99 1'
 for gl in $gae_lambdas; do 
     python cs285/scripts/run_hw2.py \
     --env_name Hopper-v4 --ep_len 1000 \
