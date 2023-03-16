073d390432e7160333011a446ea6e31b59d30612
Deok Gun Park <intuinno@gmail.com>
2023-03-16 10:54:41 -0500

    Edited runscript

--- hw3/cs285/scripts/run_question2.sh
+++ hw3/cs285/scripts/run_question2.sh
@@ -2,8 +2,8 @@
 
 seeds='1 2 3'
 for seed in $seeds; do 
-    python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_${seed} --seed $seed 
-    python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_doubledqn_${seed} --seed $seed
+    python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_${seed} --seed $seed &  
+    python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_doubledqn_${seed} --seed $seed &
 done
 
 
